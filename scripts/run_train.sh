793c19737b9c445899ff53d59a04bee33c9e5cf7
maverick0721 <shivang0721gupta@gmail.com>
2026-03-10 04:30:57 +0000

    Updated run_train.sh

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -3,7 +3,7 @@ OMP_NUM_THREADS=1 \
 torchrun \
   --nproc_per_node=4 \
   --master_port=29501 \
-  training/train_vlm.py \
+  -m training.train_vlm \
   --bf16 True \
   --tf32 True \
   --gradient_checkpointing True
