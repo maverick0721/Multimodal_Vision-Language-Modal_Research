74e7c4739f17cade257acb8d597b0bb0d0f568f4
maverick0721 <shivang0721gupta@gmail.com>
2026-03-10 08:09:36 +0000

    Updated decoder layer

--- text/decoder_layer.py
+++ text/decoder_layer.py
@@ -1,23 +1,32 @@
 import torch.nn as nn
-from .rmsnorm import RMSNorm
+
 from .gqa_attention import GQAAttention
-from .moe_ffn import MoE
+from .cross_attention import CrossAttention
+from .ffn import FFN
+from .rmsnorm import RMSNorm
+
 
 class DecoderLayer(nn.Module):
 
-    def __init__(self,dim):
+    def __init__(self, dim):
 
         super().__init__()
 
         self.norm1 = RMSNorm(dim)
-        self.norm2 = RMSNorm(dim)
-
         self.attn = GQAAttention(dim)
-        self.ffn = MoE(dim)
 
-    def forward(self,x):
+        self.norm2 = RMSNorm(dim)
+        self.cross = CrossAttention(dim)
+
+        self.norm3 = RMSNorm(dim)
+        self.ffn = FFN(dim)
+
+    def forward(self, x, vision):
 
         x = x + self.attn(self.norm1(x))
-        x = x + self.ffn(self.norm2(x))
+
+        x = x + self.cross(self.norm2(x), vision)
+
+        x = x + self.ffn(self.norm3(x))
 
         return x
