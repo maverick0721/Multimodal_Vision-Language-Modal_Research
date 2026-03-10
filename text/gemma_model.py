70e2b58d94e24762c097121efaf38ea1a996308c
maverick0721 <shivang0721gupta@gmail.com>
2026-03-10 03:39:36 +0000

    Created gemmaModel

---
+++ text/gemma_model.py
@@ -0,0 +1,31 @@
+import torch.nn as nn
+from .decoder_layer import DecoderLayer
+
+class GemmaModel(nn.Module):
+
+    def __init__(self,vocab,dim=768,depth=12):
+
+        super().__init__()
+
+        self.embed = nn.Embedding(vocab,dim)
+
+        self.layers = nn.ModuleList(
+            [DecoderLayer(dim) for _ in range(depth)]
+        )
+
+        self.norm = nn.LayerNorm(dim)
+
+        self.head = nn.Linear(dim,vocab,bias=False)
+
+        self.head.weight = self.embed.weight
+
+    def forward(self,tokens):
+
+        x = self.embed(tokens)
+
+        for layer in self.layers:
+            x = layer(x)
+
+        x = self.norm(x)
+
+        return self.head(x)
