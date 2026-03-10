89669b6ec6849e5afcc81578d00b4450c18102e3
maverick0721 <shivang0721gupta@gmail.com>
2026-03-10 03:53:25 +0000

    Controlling randomness & Parameters in sampling.py

---
+++ inference/sampling.py
@@ -0,0 +1,24 @@
+import torch
+
+def top_p(logits,p=0.9,temp=1.0):
+
+    logits = logits / temp
+
+    probs = torch.softmax(logits,-1)
+
+    sorted_probs,sorted_idx = torch.sort(
+        probs,
+        descending=True
+    )
+
+    cumulative = torch.cumsum(sorted_probs,-1)
+
+    mask = cumulative > p
+
+    sorted_probs[mask] = 0
+
+    sorted_probs /= sorted_probs.sum()
+
+    token = torch.multinomial(sorted_probs,1)
+
+    return sorted_idx[token]
